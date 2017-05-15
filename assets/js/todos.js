22e456fb658f31470bfd06dadd41d35e7b3d260d
krgiani <kr.giani@gmail.com>
2017-05-16 06:53:30 +1000

    apply styles

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,7 +3,7 @@ $("input[type='text']").keypress(function(event) {
 	if(event.which === 13) {
 		var todoText = $(this).val();
 		$(this).val('');
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 	}
 });
 
@@ -18,4 +18,8 @@ $("ul").on("click", "span", function(event) {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text']").fadeToggle();
 });
