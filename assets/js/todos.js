d3db3bb2bbfd58991c9f11761252c9df2a7835b8
krgiani <kr.giani@gmail.com>
2017-05-16 05:52:17 +1000

    function to add new to-do

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,19 @@
+// Add New To-do Text
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val('');
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
 
 // Check Off Specific Todos By Clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 // Click on X to Delete To-do
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(function() {
 		$(this).remove();
 	});
